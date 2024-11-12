7d6c3622d7adda36238ebaa88dc3fb3f2a59c2c8
Ahmad Zaki <ahmdzakiiiy@gmail.com>
2024-11-12 14:50:12 +0700

    routing

---
+++ routes.js
@@ -0,0 +1,35 @@
+const routes = [{
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => 'Homepage',
+},
+{
+  method: '*',
+  path: '/',
+  handler: (request, h) => {
+    return `Halaman tidak dapat diakses dengan method tersebut`;
+  },
+},
+{
+  method: 'GET',
+  path: '/about',
+  handler: (request, h) => {
+    return 'About page';
+  },
+},
+{
+  method: '*',
+  path: '/about',
+  handler: (request, h) => {
+    return 'Halaman tidak dapat diakses dengan method tersebut';
+  },
+},
+
+{
+  method: '*',
+  path: '/{any*}',
+  handler: (request, h) => {
+    return 'Halaman tidak ditemukan';
+  },
+},
+];
